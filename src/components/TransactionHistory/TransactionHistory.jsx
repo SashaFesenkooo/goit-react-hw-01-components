8705c790822eff2e3d3b2d8c48d6aaf21bc6a182
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-03-07 17:49:57 +0200

    .

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -13,14 +13,17 @@ return (
     </tr>
   </thead>
 
-  <tbody > { 
-  items.map( item => ( 
+  {/* <tbody > { 
+      items.map(item => {
+       console.log(item);
+        return (
+  
   <tr key={item.id} >
       <td>{item.type}</td>
       <td>{item.amount}</td>
       <td>{item.currency}</td>
-    </tr>))}
-  </tbody> 
+    </tr>)})}
+  </tbody>  */}
 </table> 
 )}
 
