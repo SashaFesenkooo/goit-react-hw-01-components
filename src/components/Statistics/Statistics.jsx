8061b4545ff07182fa0d8163fbf3c742aab8fbea
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-03-07 17:27:48 +0200

    add task

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+
+export const  Statistics = ({title, stats}) => {
+  return (
+  <section className={css.statistics}>
+   {title && <h2 className={css.title}>{title}</h2>}
+
+  <ul className={css.statList}>
+    {stats.map(elem => (
+    <li 
+    key={elem.id}
+    className={css.item}>
+    
+      <span className="label">{elem.label}</span>
+      <span className={css.percentage}>{elem.percentage}</span>
+    </li>))}
+  </ul>
+</section>)
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf (
+    PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }))
+}
